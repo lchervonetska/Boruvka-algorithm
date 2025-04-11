33d12cc5badcaf827ccc887b863cf4d09df7f8a6
mariaosi <mariaosinnaa22@gmail.com>
2025-04-11 15:49:23 +0300

    Added algorithm for adj list

--- Boruvka-algorithm/BoruvkaAlgorithm.cs
+++ Boruvka-algorithm/BoruvkaAlgorithm.cs
@@ -77,4 +77,74 @@ public class BoruvkaAlgorithm
 
         return (mstEdges, totalWeight);
     }
+    public (List<(int, int)> mstEdges, int mstWeight) BoruvkaList(List<(int to, int weight)>[] adjList)
+    {
+        int n = adjList.Length;
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        int Find(int u)
+        {
+            while (parent[u] != u)
+            {
+                u = parent[u];
+            }
+            return u;
+        }
+
+        void Union(int u, int v)
+        {
+            int rootU = Find(u);
+            int rootV = Find(v);
+            if (rootU != rootV)
+            {
+                parent[rootV] = rootU;
+            }
+        }
+
+        int components = n;
+        int totalWeight = 0;
+        var mstEdges = new List<(int, int)>();
+
+        while (components > 1)
+        {
+            var cheapest = new (int u, int v, int weight)[n];
+            for (int i = 0; i < n; i++)
+            {
+                cheapest[i] = (-1, -1, int.MaxValue);
+            }
+
+            for (int u = 0; u < n; u++)
+            {
+                int setU = Find(u);
+                foreach (var (v, weight) in adjList[u])
+                {
+                    int setV = Find(v);
+                    if (setU != setV && weight < cheapest[setU].weight)
+                    {
+                        cheapest[setU] = (u, v, weight);
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                var (u, v, weight) = cheapest[i];
+                if (u != -1 && v != -1)
+                {
+                    int setU = Find(u);
+                    int setV = Find(v);
+                    if (setU != setV)
+                    {
+                        Union(setU, setV);
+                        mstEdges.Add((u + 1, v + 1)); 
+                        totalWeight += weight;
+                        components--;
+                    }
+                }
+            }
+        }
+
+        return (mstEdges, totalWeight);
+    }
 }
--- Boruvka-algorithm/Program.cs
+++ Boruvka-algorithm/Program.cs
@@ -3,6 +3,7 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("Matrix method");
         int[,] graph = new int[,]
         {
             { 0, 2, 0, 6, 0 },
@@ -13,16 +14,35 @@ class Program
         };
 
         BoruvkaAlgorithm boruvka = new BoruvkaAlgorithm();
-        var result = boruvka.BoruvkaMatrix(graph);
-
+        var resultMatrix = boruvka.BoruvkaMatrix(graph);
         
-
         Console.WriteLine("Edges MST:");
-        foreach (var edge in result.mstEdges)
+        foreach (var edge in resultMatrix.mstEdges)
         {
             Console.WriteLine($"{edge.Item1} - {edge.Item2}");
         }
 
-        Console.WriteLine($"MST weight: {result.mstWeight}");
+        Console.WriteLine($"MST weight: {resultMatrix.mstWeight}");
+        
+        Console.WriteLine("List method");
+        var adjList = new List<(int, int)>[]
+        {
+            new() { (1, 4), (2, 3) },
+            new() { (0, 4), (2, 1), (3, 2) },
+            new() { (0, 3), (1, 1), (3, 4) },
+            new() { (1, 2), (2, 4) }
+        };
+
+        var resultList = boruvka.BoruvkaMatrix(graph);
+        
+        Console.WriteLine("Edges MST:");
+        foreach (var edge in resultList.mstEdges)
+        {
+            Console.WriteLine($"{edge.Item1} - {edge.Item2}");
+        }
+
+        Console.WriteLine($"MST weight: {resultList.mstWeight}");
+
+
     }
 }
