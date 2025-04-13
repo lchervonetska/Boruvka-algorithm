e553250816868b27b4ddbb7b685f4b982cf598a9
ddanyliuk18 <ddanyliuk@kse.org.ua>
2025-04-13 14:34:31 +0300

    Repaired graphs generator

--- Boruvka-algorithm/GraphGenerator.cs
+++ Boruvka-algorithm/GraphGenerator.cs
@@ -6,9 +6,24 @@ public class GraphGenerator
     {
         GraphAdjList graph = new GraphAdjList(n);
         int maxEdges = n * (n - 1) / 2;
-        int targetEdges = (int)(seed * maxEdges);
+        int targetEdges = Math.Max(n-1,(int)(seed * maxEdges));
         HashSet<string> adjList = new HashSet<string>();
         Random random = new Random();
+        List<int> vertices = Enumerable.Range(0, n).ToList();
+        vertices = vertices.OrderBy(x => random.Next()).ToList();
+        for(int i = 0; i < n - 1; i++)
+        {
+            int u = vertices[i];
+            int v = vertices[i + 1];
+            string edge = $"{Math.Min(u, v)} - {Math.Max(u, v)}";
+            if (!adjList.Contains(edge))
+            {
+                adjList.Add(edge);
+                int weight = random.Next(1, 11);
+                graph.AddEdge(u, v, weight);
+            }
+        }
+
         while (adjList.Count < targetEdges)
         {
             int u = random.Next(0, n);
@@ -36,6 +51,20 @@ public class GraphGenerator
         int targetEdges = (int)(seed * maxEdges);
         HashSet<string> matrix = new HashSet<string>();
         Random random = new Random();
+        List<int> vertices = Enumerable.Range(0, n).ToList();
+        vertices = vertices.OrderBy(x => random.Next()).ToList();
+        for (int i = 0; i < n - 1; i++)
+        {
+            int u = vertices[i];
+            int v = vertices[i+1];
+            string edge = $"{Math.Min(u, v)} - {Math.Max(u, v)}";
+            while (!matrix.Contains(edge))
+            {
+                matrix.Add(edge);
+                int weight = random.Next(1, 11);
+                graph.AddEdge(u, v, weight);
+            }
+        }
         while (matrix.Count < targetEdges)
         {
             int u = random.Next(0, n);
--- Boruvka-algorithm/Program.cs
+++ Boruvka-algorithm/Program.cs
@@ -3,46 +3,51 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Matrix method");
-        int[,] graph = new int[,]
-        {
-            { 0, 2, 0, 6, 0 },
-            { 2, 0, 3, 8, 5 },
-            { 0, 3, 0, 0, 7 },
-            { 6, 8, 0, 0, 9 },
-            { 0, 5, 7, 9, 0 }
-        };
+        // Console.WriteLine("Matrix method");
+        // int[,] graph = new int[,]
+        // {
+        //     { 0, 2, 0, 6, 0 },
+        //     { 2, 0, 3, 8, 5 },
+        //     { 0, 3, 0, 0, 7 },
+        //     { 6, 8, 0, 0, 9 },
+        //     { 0, 5, 7, 9, 0 }
+        // };
+        //
+        // BoruvkaAlgorithm boruvka = new BoruvkaAlgorithm();
+        // var resultMatrix = boruvka.BoruvkaMatrix(graph);
+        //
+        // Console.WriteLine("Edges MST:");
+        // foreach (var edge in resultMatrix.mstEdges)
+        // {
+        //     Console.WriteLine($"{edge.Item1} - {edge.Item2}");
+        // }
+        //
+        // Console.WriteLine($"MST weight: {resultMatrix.mstWeight}");
+        //
+        // Console.WriteLine("List method");
+        // var adjList = new List<(int, int)>[]
+        // {
+        //     new() { (1, 4), (2, 3) },
+        //     new() { (0, 4), (2, 1), (3, 2) },
+        //     new() { (0, 3), (1, 1), (3, 4) },
+        //     new() { (1, 2), (2, 4) }
+        // };
+        //
+        // var resultList = boruvka.BoruvkaMatrix(graph);
+        //
+        // Console.WriteLine("Edges MST:");
+        // foreach (var edge in resultList.mstEdges)
+        // {
+        //     Console.WriteLine($"{edge.Item1} - {edge.Item2}");
+        // }
+        //
+        // Console.WriteLine($"MST weight: {resultList.mstWeight}");
+        GraphGenerator generator = new GraphGenerator();
+        GraphAdjList graphadj = generator.GenerateRandomAdjList(10, 0.3);
+        graphadj.PrintGraphAdjList();
 
-        BoruvkaAlgorithm boruvka = new BoruvkaAlgorithm();
-        var resultMatrix = boruvka.BoruvkaMatrix(graph);
-        
-        Console.WriteLine("Edges MST:");
-        foreach (var edge in resultMatrix.mstEdges)
-        {
-            Console.WriteLine($"{edge.Item1} - {edge.Item2}");
-        }
-
-        Console.WriteLine($"MST weight: {resultMatrix.mstWeight}");
-        
-        Console.WriteLine("List method");
-        var adjList = new List<(int, int)>[]
-        {
-            new() { (1, 4), (2, 3) },
-            new() { (0, 4), (2, 1), (3, 2) },
-            new() { (0, 3), (1, 1), (3, 4) },
-            new() { (1, 2), (2, 4) }
-        };
-
-        var resultList = boruvka.BoruvkaMatrix(graph);
-        
-        Console.WriteLine("Edges MST:");
-        foreach (var edge in resultList.mstEdges)
-        {
-            Console.WriteLine($"{edge.Item1} - {edge.Item2}");
-        }
-
-        Console.WriteLine($"MST weight: {resultList.mstWeight}");
-
+        GraphMatrix matrix = generator.GenerateRandomMatrix(10, 0.3);
+        matrix.PrintGraphMatrix();
 
     }
 }
